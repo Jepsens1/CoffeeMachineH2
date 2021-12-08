7f0efad6d863a011d6532bab68bc9b856517969d
Phillip Svanholt Kjær Jepsen <jepsens123@outlook.dk>
2021-12-09 00:00:02 +0100

    commit

--- CoffeeMachine/Container.cs
+++ CoffeeMachine/Container.cs
@@ -4,14 +4,14 @@ using System.Text;
 
 namespace CoffeeMachine
 {
-    class CoffeeContainer
+    class Container
     {
         //Properties
         public double MaxCapacity { get; set; } = 2;
         public double Capacity { get; set; }
 
         //Constructor
-        public CoffeeContainer(double capacity, double maxcapacity)
+        public Container(double capacity, double maxcapacity)
         {
             Capacity = capacity;
             MaxCapacity = maxcapacity;
---
+++ CoffeeMachine/EspressoMachine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoffeeMachine
+{
+    class EspressoMachine : Machine
+    {
+        public override void AddFilter()
+        {
+            Filter filter = new Filter();
+        }
+
+        public override void FillContainer(int amount)
+        {
+            //Adds water to the container
+            Container.Capacity += amount;
+            //Checks to see if machine is full
+            if (Container.Capacity > Container.MaxCapacity)
+            {
+                //Machine is already full
+            }
+            else
+            {
+                //Filled container
+            }
+        }
+
+        public override void MakeDrink()
+        {
+            Container.Capacity -= 0.7;
+        }
+        public override string TurnOn()
+        {
+            State = true;
+            return "EspressoMachine is now on";
+        }
+        public override string TurnOff()
+        {
+            State = false;
+            return "EspressoMachine is now off";
+        }
+    }
+}
